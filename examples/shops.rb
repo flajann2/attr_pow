7f9ebf9cb3cf0686b70ef0602ffce2eedda56a00
Lachlan Priest <lachlanpriest@gmail.com>
2017-05-14 07:49:26 +0800

    Rewrite example file

---
+++ examples/shops.rb
@@ -0,0 +1,28 @@
+require 'attr_pow'
+require 'pp'
+
+class Foobar
+  attr_pow :shopper, shops: true
+
+  def customers &block
+    shopper_add &block
+  end
+
+  def purchase stype
+    shopper_shops(stype)
+  end
+end
+
+fb = Foobar.new
+fb.customers do |stype|
+  puts "Bob: #{stype} bought"
+  "bob_#{stype}"
+end
+
+fb.customers do |stype|
+  puts "James: #{stype} bought"
+  "james_#{stype}"
+end
+
+pp fb.purchase :shirt
+pp fb.purchase :pants
