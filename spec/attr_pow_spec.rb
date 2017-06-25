217031429ae20c762efddd5b20060dd8b20448ab
Fred Mitchell <fred.mitchell@gmx.de>
2017-06-25 14:10:28 +0200

    added tests for queueing and hooking

--- spec/attr_pow_spec.rb
+++ spec/attr_pow_spec.rb
@@ -15,12 +15,16 @@ describe Module do
     hook_func_add { [:h, 1] }
     hook_func_add { [:h, 2] }
     result = hook_func_hooks()
-    ap result
     expect(result.first).to eq [:h, 1]
     expect(result.last).to  eq [:h, 2]
   end
 
   it "queues" do
-
+    t = Thread.new { queue << 1 << 2 << 3; }
+    expect(queue.next).to eq 1
+    expect(queue.next).to eq 2
+    expect(queue.next).to eq 3
+    expect(queue.empty?).to be_truthy
+    t.join
   end
 end
